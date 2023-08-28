2312462eea1ed3274f3e02e50e0d6373d44e08f7
4mmZ <andresmaxi111014@gmail.com>
2023-08-28 12:36:11 -0700

    task 9: fix comments

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,4 +4,5 @@ from typing import Union, Tuple, Callable, Iterable, Sequence, List
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    " Function that takes a list as input and returns a list of tuples."
     return [(i, len(i)) for i in lst]
