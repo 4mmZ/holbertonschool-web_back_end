eca2413916876baf323eaf1d2d7f0fdcab05353d
4mmZ <andresmaxi111014@gmail.com>
2023-08-28 11:36:11 -0700

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 2. Basic annotations - floor """
+
+
+def floor(n: float = 0.0) -> int:
+    """ type-annotated function floor which takes a float n
+     as argument and returns the floor of the float."""
+    return int(n)
