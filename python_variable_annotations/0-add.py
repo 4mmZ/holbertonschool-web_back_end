9d2f9d4e866dd2d13e52863e7d7e7338f8d85bb1
4mmZ <andresmaxi111014@gmail.com>
2023-08-28 11:24:21 -0700

    Had uploaded it to the wrong repository: task 0 to 1 and readme

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+""" 0. Basic annotations - add """
+
+
+def add(a: float = 0.0, b: float = 0.0) -> float:
+    """ type-annotated function add that takes a float
+      a and a float b as arguments and returns their sum as a float. """
+    return a + b
