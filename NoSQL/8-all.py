14ba9c7191e8cb3a0f2d611d555d045b53598284
4mmZ <andresmaxi111014@gmail.com>
2023-09-05 11:16:40 -0700

    task 8: dosent work

---
+++ NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" """
+from pymongo import MongoClient
+
+def list_all(mongo_collection):
+    """ """
+    list = []
+    for x in mongo_collection.find():
+        list.append(x)
+
+    print(list)
