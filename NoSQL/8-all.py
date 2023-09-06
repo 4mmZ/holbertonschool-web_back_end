268708723d7c1e24f897e199eb8d10b0184517fe
4mmZ <andresmaxi111014@gmail.com>
2023-09-06 07:00:14 -0700

    task 8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,8 +3,10 @@
 from pymongo import MongoClient
 
 def list_all(mongo_collection):
-    """ """
+    """ Python function that lists all documents in a collection: """
     list = []
-    for x in mongo_collection.find():
-        list.append(x)
-    return list
+    result = mongo_collection.find()
+    if mongo_collection.count_documents({}) < 1:
+        return list
+    else:
+        return result
