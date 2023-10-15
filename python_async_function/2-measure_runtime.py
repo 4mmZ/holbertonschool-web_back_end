230d6b2ab52f05109f12e664557ed01472be888a
4mmZ <andresmaxi111014@gmail.com>
2023-10-15 15:23:09 -0700

    task 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """ 2. Measure the runtime """
+import asyncio
 import time
-wait_n = __import__('1-concurrent_coroutines.py').wait_n
+import random
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
 
 
 def measure_time(n, max_delay):
