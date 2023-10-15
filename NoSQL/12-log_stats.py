640b268ee2f6b29d323d7afae46b4715dde18e83
4mmZ <andresmaxi111014@gmail.com>
2023-10-15 16:37:55 -0700

    task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
-""" 
-Python script that provides some stats
-about Nginx logs stored in MongoDB 
+"""
+Python script that provides some stats about Nginx logs stored in MongoDB
 """
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://localhost:27017')
 db = client['logs']
 collection = db['nginx']
 
 total_logs = collection.count_documents({})
 
-http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {method: collection.count_documents
-                 ({"method": method}) for method in http_methods}
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+method_counts = {method: collection.count_documents({"method": method}) for method in methods}
 
-status_endpoint_count = collection.count_documents({"method": "GET", "path": "/status"})
+status_count = collection.count_documents({"method": "GET", "path": "/status"})
 
-print(f"Total logs: {total_logs} logs")
+print(f"{total_logs} logs")
 print("Methods:")
-for method in http_methods:
-    print(f"\t{method}: {method_counts[method]} logs")
-print(f"GET method with path=/status: {status_endpoint_count} logs")
+for method, count in method_counts.items():
+    print(f"\t{method}: {count} logs")
+
+print(f"method=GET path=/status: {status_count} logs")
 
 client.close()
