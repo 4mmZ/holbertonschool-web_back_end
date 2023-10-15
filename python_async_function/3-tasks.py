0c0b72461b0a09777c6e4c60e0be323cebf5040b
4mmZ <andresmaxi111014@gmail.com>
2023-10-15 15:55:54 -0700

    task 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -4,15 +4,9 @@ import asyncio
 import concurrent.futures
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-def task_wait_random(max_delay):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """ Write a function (do not create an async function,
     use the regular function syntax to do this)
     task_wait_random that takes an integer max_delay
     and returns a asyncio.Task. """
-    loop = asyncio.get_event_loop()
-    executor = concurrent.futures.ThreadPoolExecutor()
-
-    def wait_random_sync(max_delay):
-        return loop.run_in_executor(executor, wait_random, max_delay)
-
-    return asyncio.ensure_future(wait_random_sync(max_delay))
+    return asyncio.create_task(wait_random(max_delay))
