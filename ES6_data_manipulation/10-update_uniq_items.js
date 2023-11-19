36212fc3ad8747418878b3585e592ec59ce231f0
4mmZ <andresmaxi111014@gmail.com>
2023-11-19 14:05:36 -0800

    task 10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,10 @@
-export default function updateUniqueItems(updated_map) {
-  if (!(updated_map instanceof Map)) {
+export default function updateUniqueItems(updatedMap) {
+  if (!(updatedMap instanceof Map)) {
     throw new Error('Cannot process');
   }
-  updated_map.forEach((value, key) => {
+  updatedMap.forEach((value, key) => {
     if (value === 1) {
-      updated_map.set(key, 100);
+      updatedMap.set(key, 100);
     }
   });
 }
