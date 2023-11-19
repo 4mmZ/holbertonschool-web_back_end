66772cebac0d7826abdf3ffdbb38ce4dd856bc1f
4mmZ <andresmaxi111014@gmail.com>
2023-11-19 14:01:09 -0800

    task 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const grocery_list = new Map();
+  grocery_list.set('Apples', 10);
+  grocery_list.set('Tomatoes', 10);
+  grocery_list.set('Pasta', 1);
+  grocery_list.set('Rice', 1);
+  grocery_list.set('Banana', 5);
+  return grocery_list;
+}
